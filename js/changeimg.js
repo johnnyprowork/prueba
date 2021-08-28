13b79cec5828deaaf15e316380dcbd70d967d653
johnny Argote <johnnyargpro@outlook.es>
2021-08-28 00:03:12 -0500

    carga 1.0

---
+++ js/changeimg.js
@@ -0,0 +1,44 @@
+
+$('#name2 img').hover(function() {
+ $(this).attr('src', 'icons/Language_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/Language.svg', );
+}); 
+
+$('#name3 img').hover(function() {
+ $(this).attr('src', 'icons/Notification_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/Notification.svg', );
+}); 
+
+$('#name4 img').hover(function() {
+ $(this).attr('src', 'icons/User_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/User.svg', );
+}); 
+
+$('#name5 img').hover(function() {
+ $(this).attr('src', 'icons/BUp_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/BUp.svg', );
+}); 
+
+$('#name6 img').hover(function() {
+ $(this).attr('src', 'icons/BDown_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/BDown.svg', );
+}); 
+
+
+$('#name7 img').hover(function() {
+ $(this).attr('src', 'icons/BDown_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/BDown.svg', );
+}); 
+
+
+$('#name8 img').hover(function() {
+ $(this).attr('src', 'icons/BLeft_active.svg', );
+}, function() {
+$(this).attr('src', 'icons/BLeft.svg', );
+}); 
---
+++ js/onclick.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.dashboard_img').click(function(){
+		$('.menu2').toggleClass('active')
+	})
+})
+
+$(document).ready(function(){
+	$('.dashboard_img').click(function(){
+		$(this).attr('src', 'icons/Dashboard_active.svg', );
+		$(this).attr('class', 'dashboard_img_active', );
+
+
+	$('.dashboard_img').click(function(){
+		$(this).attr('src', 'icons/Dashboard_active.svg', );
+		$(this).attr('class', 'dashboard_img_active', );
+		})
+
+	})
+
+
+})
+
+$(document).ready(function(){
+	$('.bleft').click(function(){
+		$('.menu2').toggleClass('active')
+	})
+})
+
+
+
+
+
+
+
